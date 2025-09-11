a640ee242d6f7dff59da30b73d4f03a4bee296e1
Chinmay Kaushik Parasa <chinmaykaushikparasa@gmail.com>
2025-09-11 23:52:13 +0530

    11/9

---
+++ Search a 2D Matrix.py
@@ -0,0 +1,8 @@
+#Search a 2D Matrix
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False
